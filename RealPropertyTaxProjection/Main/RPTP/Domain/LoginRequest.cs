dbee1c0a1e84b7007cb1501990b34f3a4d198f12
Arvin Mendoza <arvinsmendoza@hotmail.com>
2018-08-11 22:57:56 +0800

    Add project files.

---
+++ RealPropertyTaxProjection/Main/RPTP/Domain/LoginRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main.RPTP.Domain
+{
+    public class LoginRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public LoginRequest()
+        {
+            this.UserName = string.Empty;
+            this.Password = string.Empty;
+        }
+    }
+}
